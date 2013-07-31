fcedc2727a8c3ab9af031ad13f42340f8e272be6
Igor Ramadas <igor@devv.com>
2013-07-31 11:20:38 +0200

    Fixed Google Analytics.

--- templatescripts.js
+++ templatescripts.js
@@ -33,19 +33,7 @@ function appendCssAndJs() {
     siteCss.rel = "stylesheet";
     siteCss.href = "styles.css";
 
-    // Load site specific scripts and Google Analytics.
-    $.getScript("scripts.js", function(data, status, xhr) {
-        var gaJs = document.createElement("script");
-        gaJs.innerHTML = '' +
-            '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){' +
-            '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
-            'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
-            '})(window,document,"script","//www.google-analytics.com/analytics.js","ga");' +
-            'ga("create", "' + zalDomain + '", "' + gaId + '");' +
-            'ga("send", "pageview");';
-
-        head.appendChild(gaJs);
-    });
+    $.getScript("scripts.js");
 
     head.appendChild(templateCss);
     head.appendChild(siteCss);
