390135881b4b9197030602681b41edcf776bdbde
Igor Ramadas <igor@devv.com>
2013-10-24 15:54:24 +0200

    Updated syntax highlighter and styles.

--- templatescripts.js
+++ templatescripts.js
@@ -25,11 +25,6 @@ $(document).ready(function()
     if ($window.innerWidth() < $body.innerWidth() - 1) {
         $body.css("zoom", 0.75);
     }
-
-    // Enable syntax highlighter?
-    if (zalSH && zalSH != "") {
-        SyntaxHighlighter.all();
-    }
 });
 
 // Append JS and CSS dependencies.
@@ -49,22 +44,7 @@ function appendCssAndJs() {
     head.appendChild(siteCss);
 
     // Load scripts.js file and check for syntax highlighting.
-    $.getScript("scripts.js", function() {
-        if (zalSH && zalSH != "") {
-            zalSH = zalSH.split(",");
-
-            $.getScript("template/shCore.js");
-
-            var shCssCore = document.createElement("link");
-            shCssCore.rel = "stylesheet";
-            shCssCore.href = "template/shCoreDefault.css";
-            head.appendChild(shCssCore);
-
-            for (var i = 0; i < zalSH.length; i++) {
-                $.getScript("template/shBrush" + zalSH[i] + ".js");
-            }
-        }
-    });
+    $.getScript("scripts.js");
 }
 
 // Append CSS and JS away!
